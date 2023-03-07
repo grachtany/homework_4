5fa06caa1548473a0172dcc3f41a960c910a7c8d
tatangracheva <grachtany@yandex.ru>
2023-03-07 20:06:51 +0300

    homework 5

--- home_5/5_1.py
+++ home_5/5_1.py
@@ -1,7 +1,10 @@
-def stepen (a,b):
+def stepen(a, b):
     if b == 0:
         return 1
+    if b < 0:
+        return stepen(a, b + 1)*1 / a
     return stepen(a, b-1) * a
+
 
 A = int(input('Введите число, которое будем возводить в степень: '))
 D = int(input('Введите степень: '))
